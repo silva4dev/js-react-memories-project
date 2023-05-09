550cfe95ce9eb34aafef13ffa531207ee43d89e8
silva4dev <web4dev.lucas@outlook.com>
2023-05-09 02:49:02 -0300

    inicial

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.listen(3333, () => {
+    console.log("Server listening on http://localhost:3333");
+});
