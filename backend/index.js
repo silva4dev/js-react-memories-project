fe6c6779264ae45f6533ade029f502ebf03f4041
silva4dev <web4dev.lucas@outlook.com>
2023-05-10 03:34:27 -0300

    inicial

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,24 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from "./routes/posts.js";
+
 const app = express();
+
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.listen(3333, () => {
-    console.log("Server listening on http://localhost:3333");
+const CONNECTION_URL = "mongodb+srv://lucasasilva:lucasasilva123@cluster.ticj811.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 3333;
+
+mongoose.connect(CONNECTION_URL).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
+}).catch((error) => {
+    console.log(error);
 });
+
