5610cd0b2edf51f854a963527e8367ac63231a33
silva4dev <web4dev.lucas@outlook.com>
2023-05-20 17:30:04 -0300

    inicial

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
@@ -7,20 +8,17 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors());
 
-const CONNECTION_URL = "mongodb+srv://lucasasilva:lucasasilva123@cluster.ticj811.mongodb.net/?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 3333;
+app.use("/posts", postRoutes);
 
-mongoose.connect(CONNECTION_URL).then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server running on http://localhost:${PORT}`);
-    });
-}).catch((error) => {
-    console.log(error);
-});
+const CONNECTION_URL = "mongodb+srv://lucasasilva:lucasalves9090@cluster.ticj811.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
 
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set("useFindAndModify", false);
